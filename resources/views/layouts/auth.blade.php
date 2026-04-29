c2f8d46aa33a713a743d734d581180f0e5b17c21
mzalisko <zaliskomykola@gmail.com>
2026-04-29 07:26:53 +0300

    style(auth): vibeB class + Google Fonts in auth layout

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="uk" data-theme="dark">
+<html lang="uk" class="vibeB vibe">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'DataBridge CRM')</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
 </head>
 <body class="auth-body">
